d8515c851724ba90153b08f2c454f0ff5ba9831c
Adam Brannon <adam1320@rocketmail.com>
2022-02-21 10:08:33 -0500

    initial commit

---
+++ ParcticeLinq/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> videoGames = new List<string>() { "RaceDriver", "FirstPersonShooter", "Basketball", "Football", "Soccer", "IceHockey", "KitchenChef", "MushroomLand"};
+
+           var sortedVideoGames = videoGames.OrderBy(x => x.Length);
+                foreach (var game in sortedVideoGames)
+                {
+                    Console.WriteLine($"{game}");
+                }
+
+        }
+     
+    }
+
+}
